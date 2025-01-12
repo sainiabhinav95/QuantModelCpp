624101631c7dcd35039fdeb3ac73ccc90c9ef552
Abhinav Saini <sainiabhinav95@gmail.com>
2025-01-12 13:14:45 +0530

    Added volatilities

--- ir_models/bdt/bdt.cc
+++ ir_models/bdt/bdt.cc
@@ -4,14 +4,17 @@
 
 int main(){
     // Input Data: An array of zero coupon yields along with volatilities
-    auto input_ts = std::vector<std::pair<std::uint32_t, float>>{
-        {1, 0.05}, {2, 0.06}, {3, 0.06}, {4, 0.05}, {5, 0.06}, {6, 0.06},
-        {7, 0.05}, {8, 0.06}, {9, 0.06}, {10, 0.05}
+    auto input_ts = std::vector<std::pair<std::uint32_t, std::pair<float, float>>>{
+        {1, {0.0475, 0.15}}, {2, {0.0525, 0.16}}, {3, {0.0564, 0.17}}, {4, {0.057, 0.14}},
+         {5, {0.058, 0.14}}, {6, {0.061, 0.11}}, {7, {0.062, 0.12}}, {8, {0.064, 0.13}},
+          {9, {0.0645, 0.13}}, {10, {0.0645, 0.13}}
         };
     std::cout<<"Input Term Structure:\n";
     for (auto &v : input_ts){
-        auto &[mat, yield] = v;
-        std::cout<<"Maturity(Years): " << mat << " Yield (%): " << yield * 100 << "\n";
+        auto &[mat, ts] = v;
+        std::cout<<"Maturity(Years): " << mat <<
+        " Yield (%): " << ts.first * 100 <<
+        " Volatility (%): " << ts.second * 100 << "\n";
     }
 
     return 0;
